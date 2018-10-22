1523f1b82ee9a00a7a4cd3636aaacf81fd659377
danfate <danfate@users.noreply.github.com>
2018-10-22 23:11:15 +0800

    add obfs support

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,8 +3,9 @@
 #------------------------------------------------------------------------------
 # Configure the service:
 #------------------------------------------------------------------------------
-env sslocal -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD \
-  -b 0.0.0.0 -l ${LOCAL_PORT:-7070} -m ${METHOD:-'chacha20'} \
+env obfs-local -s $SERVER_ADDR -p $SERVER_PORT -l 19840 --obfs http --obfs-host www.bing.com
+env sslocal -s 127.0.0.1 -p 19840 -k $PASSWORD \
+  -b 0.0.0.0 -l ${LOCAL_PORT:-7070} -m ${METHOD:-'chacha20-ietf-poly1305'} \
   -d start
 
 env /usr/sbin/privoxy --no-daemon /data/config
